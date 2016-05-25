354fcc4a3fad0b928fd8561e53f5871ca955c1b0
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2016-05-25 15:47:32 +0100

    Make url endpoint to be tested configurable
    https://github.com/EnvironmentAgency/quke/issues/8
    
    Currently Quke and its examples have been built on the premise that the website undertest would be at a fixed address set within the test code. However initial feedback has shown this may not always be the case, for example QA may want to test against its own QA environment, then pre-prod, and then production upon release of the service.
    
    This change will add the ability for the user to configure the url using an environment variable.
    
    (Resolves issue #8)

--- features/support/env.rb
+++ features/support/env.rb
@@ -23,6 +23,15 @@ require 'site_prism'
 # selectors in your step definitions to use the XPath syntax.
 # Capybara.default_selector = :xpath
 
+# Normally Capybara expects to be testing an in-process Rack application, but
+# we're using it to talk to a remote host. Users of quke can set what this will
+# be by simply setting the environment variable APP_HOST. An example would be
+# APP_HOST='https://en.wikipedia.org/wiki'. You can then use it directly using
+# Capybara `visit('/Main_Page')` or `visit('/')`. In your page_objects
+# `set_url '/Main_Page'`
+$app_host = (ENV['APP_HOST'] || '')
+Capybara.app_host = $app_host
+
 # Here we are registering the poltergeist driver with capybara. There are a
 # number of options for how to configure poltergeist, and we can even pass
 # on options to phantomjs to configure how it runs
